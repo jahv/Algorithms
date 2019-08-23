f853de80d663477a6b4f170607f1633936d35d15
Jose Antonio Hernandez <ahernandez@nearsoft.com>
2019-08-23 17:38:01 -0500

    BackUp

---
+++ src/main/java/mit/algorithms/PeakFinder.java
@@ -0,0 +1,12 @@
+package mit.algorithms;
+
+/**
+ * Given a one dimensional array. a pick is the number which is greater or equals
+ * than the left and right numbers
+ */
+public class PeakFinder {
+
+    public void straighForward(int array[]) {
+
+    }
+}
